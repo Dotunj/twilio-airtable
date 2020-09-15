23696d807f41bb45d8904ff298d68f46b95f36e9
Dotun <dotunjolaosho@gmail.com>
2020-09-15 18:18:43 +0100

    added procfile for heroku deployment

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use Airtable;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class LogController extends Controller
 {
     public function logSms(Request $request)
     {
+        Log::info('Incoming Message');
+        
         $airtable = Airtable::table('twilio_sms_logs')->create([
             'Message_Sid' => $request->input('MessageSid'),
             'From' => $request->input('From'),
